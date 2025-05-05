72bfde8208c65b991e9db4132029cbd5b91cd240
ayushagrawal <ayushmaserati@gmail.com>
2025-05-05 19:36:19 +0530

    dependencies for retrofit, datastore, hilt, coroutines, navigation

--- app/src/main/java/com/gdg/zealicon2k25/presentation/ui/ContactUsScreen.kt
+++ app/src/main/java/com/gdg/zealicon2k25/presentation/ui/ContactUsScreen.kt
@@ -1,9 +1,7 @@
-package com.gdg.zealicon2k25
+package com.gdg.zealicon2k25.presentation.ui
 
-import android.graphics.drawable.shapes.Shape
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,10 +20,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.gdg.zealicon2k25.R
 import com.gdg.zealicon2k25.presentation.ui.theme.BackgroundColor
 import com.gdg.zealicon2k25.presentation.ui.theme.HeadingTextColor
 import com.gdg.zealicon2k25.presentation.ui.theme.Outfit
-import com.gdg.zealicon2k25.presentation.ui.theme.cardTextColor
 
 @Composable
 @Preview
